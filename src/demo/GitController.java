103ed2bf5a63546cf95969a9335486cede9c01e2
DJX <ding_jianxiang@163.com>
2015-09-22 22:29:31 +0800

    bbbb

--- src/demo/GitController.java
+++ src/demo/GitController.java
@@ -5,6 +5,12 @@ import org.omg.Messaging.SyncScopeHelper;
 import com.jfinal.render.Render;
 
 public class GitController {
+	public  void  add(){
+		
+	}
+	
+	
+	
          public void index(){
          }
 }
